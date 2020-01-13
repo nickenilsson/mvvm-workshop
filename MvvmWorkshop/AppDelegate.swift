e7e83d62321e36667435aa93448085bdbd5c6073
Niklas Nilsson <nilsson.sthlm@gmail.com>
2020-01-13 10:43:44 +0100

    initial commit

---
+++ MvvmWorkshop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MvvmWorkshop
+//
+//  Created by Niklas Nilsson on 2020-01-13.
+//  Copyright © 2020 Sveriges Radio AB. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let vc = ViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = vc
+
+        return true
+    }
+
+
+}
+
---
+++ MvvmWorkshop/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  MvvmWorkshop
+//
+//  Created by Niklas Nilsson on 2020-01-13.
+//  Copyright © 2020 Sveriges Radio AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    let dummyData = [
+        "tomas",
+        "niklas",
+        "björn"
+    ]
+
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.dataSource = self
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 100
+        return tv
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "MVVM workshop"
+        setupViews()
+    }
+
+    override func loadView() {
+        view = UIView()
+        super.loadView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    private func setupViews() {
+        view.addSubview(tableView)
+        view.addConstraints([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = dummyData[indexPath.item]
+        return cell
+    }
+
+}
+
